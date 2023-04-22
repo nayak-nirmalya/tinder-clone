ebe1222d0a6dc3886e10b64e0f98f1b4e336c086
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-22 11:59:00 +0530

    only show login screen if user not present

--- StackNavigator.tsx
+++ StackNavigator.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import HomeScreen from "./screens/HomeScreen";
 import ChatScreen from "./screens/ChatScreen";
 import LogInScreen from "./screens/LogInScreen";
@@ -13,29 +14,36 @@ export type RootStackParamList = {
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const StackNavigator = () => {
+  const user = false;
+
   return (
     <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          headerShown: false
-        }}
-      />
-      <Stack.Screen
-        name="Chat"
-        component={ChatScreen}
-        options={{
-          headerShown: false
-        }}
-      />
-      <Stack.Screen
-        name="LogIn"
-        component={LogInScreen}
-        options={{
-          headerShown: false
-        }}
-      />
+      {user ? (
+        <>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              headerShown: false
+            }}
+          />
+          <Stack.Screen
+            name="Chat"
+            component={ChatScreen}
+            options={{
+              headerShown: false
+            }}
+          />
+        </>
+      ) : (
+        <Stack.Screen
+          name="LogIn"
+          component={LogInScreen}
+          options={{
+            headerShown: false
+          }}
+        />
+      )}
     </Stack.Navigator>
   );
 };
