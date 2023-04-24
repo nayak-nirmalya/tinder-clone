e474b68137e8687eb009e73ece1dca30146ef488
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-24 11:33:16 +0530

    added google sign in btn

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,15 +1,10 @@
 import React, { createContext, useContext, useEffect } from "react";
 
-import * as WebBrowser from "expo-web-browser";
-import * as Google from "expo-auth-session/providers/google";
-
 import {
   ANDROID_OAUTH_CLIENT_ID_FIREBASE,
   IOS_OAUTH_CLIENT_ID_FIREBASE,
   EXPO_CLIENT_ID
 } from "@env";
-
-import { AuthSessionResult } from "expo-auth-session";
 
 interface AuthProps {
   children?: React.ReactNode;
@@ -17,64 +12,25 @@ interface AuthProps {
 
 interface AuthContextType {
   user: string | null;
-  promptAsync: () => Promise<AuthSessionResult>;
+  promptAsync: () => void;
   signInWithGoogle: () => void;
 }
 
 const AuthContext = createContext<AuthContextType>({
   user: "",
   signInWithGoogle: () => {},
-  promptAsync: () => new Promise(() => {})
+  promptAsync: () => {}
 });
-
-WebBrowser.maybeCompleteAuthSession();
 
 export const AuthProvider: React.FC<React.PropsWithChildren> = ({
   children
 }) => {
-  const [request, response, promptAsync] = Google.useAuthRequest({
-    androidClientId: ANDROID_OAUTH_CLIENT_ID_FIREBASE,
-    iosClientId: IOS_OAUTH_CLIENT_ID_FIREBASE,
-    expoClientId: EXPO_CLIENT_ID,
-    scopes: ["profile", "email"]
-  });
-
-  const signInWithGoogle = async () => {
-    if (response?.type === "success") {
-      const { idToken, accessToken } = response.authentication!;
-
-      try {
-        const userInfo = await fetch(
-          "https://www.googleapis.com/userinfo/v2/me?prettyPrint=true&fields=email,name,gender,id,link,locale,picture,family_name,given_name,hd,verified_email",
-          {
-            headers: {
-              Authorization: `Bearer ${accessToken}`
-            }
-          }
-        );
-
-        const user = await userInfo.json();
-        console.log(user);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-
-    // return Promise.reject();
-  };
-
-  // useEffect(() => {
-  //   if (!response) return;
-
-  //   signInWithGoogle();
-  // }, [response]);
-
   return (
     <AuthContext.Provider
       value={{
         user: null,
-        signInWithGoogle,
-        promptAsync
+        signInWithGoogle: () => {},
+        promptAsync: () => {}
       }}
     >
       {children}
--- screens/LogInScreen.tsx
+++ screens/LogInScreen.tsx
@@ -1,15 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, Button } from "react-native";
 import useAuth from "../hooks/useAuth";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import {
+  GoogleSignin,
+  GoogleSigninButton
+} from "@react-native-google-signin/google-signin";
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
+
 const LogInScreen = () => {
-  const { promptAsync, signInWithGoogle, user } = useAuth();
+  GoogleSignin.configure({
+    webClientId:
+      "764700743220-9l8icu5lr1b1h0uqlj6dqoghv4l3ht03.apps.googleusercontent.com"
+  });
+
+  const {} = useAuth();
+
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User>();
+
+  // Handle user state changes
+  const onAuthStateChanged = (user: FirebaseAuthTypes.User) => {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  };
+
+  const onGoogleButtonPress = async () => {
+    // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+    // Get the users ID token
+    const { idToken } = await GoogleSignin.signIn();
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    const userSignIn = auth().signInWithCredential(googleCredential);
+    userSignIn
+      .then((userInfo) => console.log(userInfo))
+      .catch((error) => console.error(error));
+  };
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <View>
+        <Text>Login</Text>
+        <GoogleSigninButton onPress={onGoogleButtonPress} />
+      </View>
+    );
+  }
 
   return (
-    <SafeAreaView className="bg-gray-500">
-      <Text className="text-xl text-center text-black">LogInScreen</Text>
-      <Button title="Sign In!" onPress={() => promptAsync()} />
+    <SafeAreaView>
+      <Text className="text-xl text-center mt-32 mb-40 text-black">
+        Welcome {user.displayName}
+      </Text>
     </SafeAreaView>
   );
 };
