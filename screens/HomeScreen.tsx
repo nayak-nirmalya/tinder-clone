5ca06b9bf0d7713ced0dd20949453f45b02f52ef
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-25 20:05:37 +0530

    added dummy data for development

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,45 @@
 import React from "react";
+import Swiper from "react-native-deck-swiper";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { AntDesign, Entypo, Ionicons } from "@expo/vector-icons";
 import { View, Text, Button, TouchableOpacity, Image } from "react-native";
 
 import useAuth from "../hooks/useAuth";
+
+const DUMMY_DATA = [
+  {
+    firstName: "Nirmalya",
+    lastName: "Nayak",
+    age: 25,
+    occupation: "Software Developer",
+    photoURL: "https://i.redd.it/fbvdmvpf3qv81.jpg"
+  },
+  {
+    firstName: "Swadesh",
+    lastName: "Nayak",
+    age: 26,
+    occupation: "IAS",
+    photoURL:
+      "https://qph.cf2.quoracdn.net/main-qimg-4920a745d6ed136ca5155062f6037197"
+  },
+  {
+    firstName: "Saroj",
+    lastName: "Kumar",
+    age: 25,
+    occupation: "Teacher",
+    photoURL:
+      "https://media.sciencephoto.com/image/f0283895/800wm/F0283895-Portrait_male_high_school_teacher_in_classroom.jpg"
+  },
+  {
+    firstName: "Kiara",
+    lastName: "Advani",
+    age: 30,
+    occupation: "Software Developer",
+    photoURL:
+      "https://upload.wikimedia.org/wikipedia/commons/6/67/Kiara_Advani_walks_for_Shyamal-Bhumika_at_India_Couture_Week_2018_Day_4_%2803%29_%28cropped%29.jpg"
+  }
+];
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -38,6 +73,16 @@ const HomeScreen = () => {
         </TouchableOpacity>
       </View>
       {/* END OF HEADER */}
+
+      {/* CARD */}
+      <Swiper
+        cards={DUMMY_DATA}
+        renderCard={(card) => (
+          <View>
+            <Text>{card.firstName}</Text>
+          </View>
+        )}
+      />
     </SafeAreaView>
   );
 };
