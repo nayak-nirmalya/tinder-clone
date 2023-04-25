e5f57d6693920e9f2e5c2dc1876043c805272257
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-25 13:19:38 +0530

    added tinder icon to homescreen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -11,8 +11,8 @@ const HomeScreen = () => {
   return (
     <SafeAreaView>
       {/* HEADER */}
-      <View>
-        <TouchableOpacity>
+      <View className="items-center relative">
+        <TouchableOpacity className="absolute left-5 top-3">
           <Image
             className="h-10 w-10 rounded-full"
             source={{
@@ -20,11 +20,15 @@ const HomeScreen = () => {
             }}
           />
         </TouchableOpacity>
+
+        <TouchableOpacity className="top-3">
+          <Image
+            className="h-10 w-10 rounded-full"
+            source={require("../assets/tinder_logo.png")}
+          />
+        </TouchableOpacity>
       </View>
       {/* END OF HEADER */}
-
-      <Button title="Chat" onPress={() => navigation.navigate("Chat")} />
-      <Button title="Sign Out" onPress={signOut} />
     </SafeAreaView>
   );
 };
