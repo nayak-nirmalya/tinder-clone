fdfda4063a12aff44cb7883d0fd64e2a552ce258
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-26 07:54:19 +0530

    showing tinder banner and welcom user msg on modal

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -1,12 +1,31 @@
-import { View, Text } from "react-native";
+import { View, Image, Text } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import useAuth from "../hooks/useAuth";
 
 const ModalScreen = () => {
+  const { user } = useAuth();
+
   return (
-    <SafeAreaView className="mt-10 bg-white h-full w-full rounded-xl">
-      <Text>ModalScreen</Text>
-    </SafeAreaView>
+    <View
+      className="
+            mt-10 bg-white 
+            h-full w-full 
+            rounded-xl 
+            pt-1 flex-1 
+            items-center
+        "
+    >
+      <Image
+        className="h-20 w-full"
+        resizeMode="contain"
+        source={{ uri: "https://links.papareact.com/2pf" }}
+      />
+
+      <Text className="text-xl text-gray-500 font-bold p-2">
+        Welcome {user?.displayName}
+      </Text>
+    </View>
   );
 };
 
