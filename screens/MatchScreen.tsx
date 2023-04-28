c6e623bcd4b0f5f39b0b973aad12220f839e0e7f
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-28 10:58:01 +0530

    on press navigate to chat screen

--- screens/MatchScreen.tsx
+++ screens/MatchScreen.tsx
@@ -20,16 +20,19 @@ const MatchScreen = ({ route, navigation }: MatchedProps) => {
   const { loggedInProfile, userSwiped } = route.params;
 
   return (
-    <View className="h-full bg-red-500 pt-20" style={{ opacity: 0.89 }}>
-      <View className="justify-center px-10 pt-20">
-        <Image source={{ uri: "https://links.papareact.com/mg9" }} />
+    <View className="h-full bg-red-500 pt-14" style={{ opacity: 0.92 }}>
+      <View className="justify-center px-10 pt-16">
+        <Image
+          className="h-20 w-full"
+          source={{ uri: "https://links.papareact.com/mg9" }}
+        />
       </View>
 
-      <Text className="text-white text-center mt-5">
+      <Text className="text-white text-center mt-12 font-semibold">
         You & {userSwiped.displayName} Have Liked Each Other.
       </Text>
 
-      <View className="flex-row justify-evenly mt-5">
+      <View className="flex-row justify-evenly mt-6">
         <Image
           className="h-32 w-32 rounded-full"
           source={{
@@ -43,6 +46,21 @@ const MatchScreen = ({ route, navigation }: MatchedProps) => {
           }}
         />
       </View>
+
+      <TouchableOpacity
+        className="
+          bg-white
+          m-14 mt-56
+          px-6 py-4
+          rounded-full
+        "
+        onPress={() => {
+          navigation.goBack();
+          navigation.navigate("Chat");
+        }}
+      >
+        <Text className="text-center font-bold text-md">Send a Message</Text>
+      </TouchableOpacity>
     </View>
   );
 };
