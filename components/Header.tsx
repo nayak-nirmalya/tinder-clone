ef83187f9e7cfbba4df966bde1a19187248446f3
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-28 11:12:49 +0530

    added btn in header compe to goback

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,24 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { Foundation, Ionicons } from "@expo/vector-icons";
+import { View, Text, TouchableOpacity } from "react-native";
 
 interface HeaderProps {
   title: string;
+  callEnabled?: boolean;
 }
 
-const Header: React.FC<HeaderProps> = ({ title }) => {
+const Header: React.FC<HeaderProps> = ({ title, callEnabled = false }) => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>{title}</Text>
+    <View className="p-2 flex-row items-center justify-between">
+      <View className="flex flex-row items-center">
+        <TouchableOpacity className="p-2" onPress={() => navigation.goBack()}>
+          <Ionicons name="chevron-back-outline" size={34} color="#FF5864" />
+        </TouchableOpacity>
+        <Text className="text-2xl font-bold pl-2">{title}</Text>
+      </View>
     </View>
   );
 };
