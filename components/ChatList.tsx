2b93f98c33cc0c64ca806e0dfc82c1c9e6674460
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-28 11:59:23 +0530

    show no matches if matches collection is empty

--- components/ChatList.tsx
+++ components/ChatList.tsx
@@ -1,10 +1,44 @@
-import { View, Text } from "react-native";
-import React from "react";
+import firestore, {
+  FirebaseFirestoreTypes
+} from "@react-native-firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { View, Text, Image, SafeAreaView, FlatList } from "react-native";
+
+import useAuth from "../hooks/useAuth";
+import Header from "../components/Header";
+import { Profile } from "../screens/HomeScreen";
+
+interface Match {
+  id: string;
+  users: Profile[];
+  usersMatched: string[];
+  timestamp: FirebaseFirestoreTypes.FieldValue;
+}
 
 const ChatList = () => {
-  return (
-    <View>
-      <Text>ChatList</Text>
+  const { user } = useAuth();
+  const [matches, setMatches] = useState<Match[]>([]);
+
+  useEffect(() => {
+    firestore()
+      .collection("Matches")
+      .where("usersMatched", "array-contains", user?.uid)
+      .get()
+      .then((snapshot) =>
+        setMatches(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data()
+          })) as Match[]
+        )
+      );
+  }, [user?.uid]);
+
+  return matches.length > 0 ? (
+    <FlatList />
+  ) : (
+    <View className="p-6">
+      <Text className="text-center text-lg">No Matches At the Moment. :(</Text>
     </View>
   );
 };
--- screens/ChatScreen.tsx
+++ screens/ChatScreen.tsx
@@ -1,40 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { View, Text, Image, SafeAreaView } from "react-native";
-import firestore, {
-  FirebaseFirestoreTypes
-} from "@react-native-firebase/firestore";
+import React from "react";
+import { SafeAreaView } from "react-native";
 
-import { Profile } from "./HomeScreen";
-import useAuth from "../hooks/useAuth";
 import Header from "../components/Header";
 import ChatList from "../components/ChatList";
 
-interface Match {
-  id: string;
-  users: Profile[];
-  usersMatched: string[];
-  timestamp: FirebaseFirestoreTypes.FieldValue;
-}
-
 const ChatScreen = () => {
-  const { user } = useAuth();
-  const [matches, setMatches] = useState<Match[]>([]);
-
-  useEffect(() => {
-    firestore()
-      .collection("Matches")
-      .where("usersMatched", "array-contains", user?.uid)
-      .get()
-      .then((snapshot) =>
-        setMatches(
-          snapshot.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data()
-          })) as Match[]
-        )
-      );
-  }, []);
-
   return (
     <SafeAreaView>
       <Header title="Chat" callEnabled />
