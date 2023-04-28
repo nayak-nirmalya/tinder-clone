c86a12fb897861b7856a012410aceb8f61d971fc
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-28 08:39:22 +0530

    added new screen match

--- StackNavigator.tsx
+++ StackNavigator.tsx
@@ -3,16 +3,18 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import HomeScreen from "./screens/HomeScreen";
 import ChatScreen from "./screens/ChatScreen";
+import MatchScreen from "./screens/MatchScreen";
 import LogInScreen from "./screens/LogInScreen";
+import ModalScreen from "./screens/ModalScreen";
 
 import useAuth from "./hooks/useAuth";
-import ModalScreen from "./screens/ModalScreen";
 
 export type RootStackParamList = {
   Home: undefined;
   Chat: undefined;
   LogIn: undefined;
   Modal: undefined;
+  Match: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -30,6 +32,7 @@ const StackNavigator = () => {
           <Stack.Group>
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Chat" component={ChatScreen} />
+            <Stack.Screen name="Match" component={MatchScreen} />
           </Stack.Group>
 
           <Stack.Group
