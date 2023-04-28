ad87dbec93e67644142870b81e1d71c7a0cb62ed
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-28 11:18:09 +0530

    added new component ChatList

--- screens/ChatScreen.tsx
+++ screens/ChatScreen.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { View, Text, SafeAreaView } from "react-native";
 import Header from "../components/Header";
+import ChatList from "../components/ChatList";
 
 const ChatScreen = () => {
   return (
     <SafeAreaView>
-      <Header title="Chat" />
+      <Header title="Chat" callEnabled />
+      <ChatList />
     </SafeAreaView>
   );
 };
