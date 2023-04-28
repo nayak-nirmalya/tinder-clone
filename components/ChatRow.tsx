a4b0f399d589bd5d3e79dda6656a0d162d6e4fb6
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-28 12:39:18 +0530

    added chat screen photo

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -4,6 +4,7 @@ import { View, Text, Image, TouchableOpacity } from "react-native";
 
 import useAuth from "../hooks/useAuth";
 import { Match } from "../lib/typesInterfaces";
+import getMatchedUserInfo from "../lib/getMatchedUserInfo";
 
 export interface ChatRowProps {
   matchDetails: Match;
@@ -14,9 +15,29 @@ const ChatRow: React.FC<ChatRowProps> = ({ matchDetails }) => {
   const navigation = useNavigation();
   const [matchedUserInfo, setMatchedUserInfo] = useState(null);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    setMatchedUserInfo(getMatchedUserInfo(matchDetails.users, user?.uid!));
+  }, [matchDetails, user]);
 
-  return <TouchableOpacity></TouchableOpacity>;
+  return (
+    <TouchableOpacity
+      className="
+        flex-row 
+        items-center 
+        py-3 px-5 
+        bg-white 
+        mx-3 my-1 
+        rounded-lg
+      "
+    >
+      <Image
+        source={{
+          uri: matchedUserInfo?.photoURL!
+        }}
+        className="rounded-full h-16 w-16 mr-4 shadow-2xl"
+      />
+    </TouchableOpacity>
+  );
 };
 
 export default ChatRow;
