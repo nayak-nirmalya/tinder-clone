bfca5ef08306648404c7ac3cd80f5119be3c2912
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-29 07:17:11 +0530

    show user display name on header

--- screens/MessageScreen.tsx
+++ screens/MessageScreen.tsx
@@ -1,10 +1,13 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import useAuth from "../hooks/useAuth";
 import Header from "../components/Header";
-import { Match } from "../lib/typesInterfaces";
+import getUserById from "../lib/getMatchedUserInfo";
 import { RootStackParamList } from "../StackNavigator";
+import { Match, Profile } from "../lib/typesInterfaces";
 
 export interface MessageScreenProps {
   matchDetails: Match;
@@ -13,13 +16,25 @@ export interface MessageScreenProps {
 type MessageProps = NativeStackScreenProps<RootStackParamList, "Message">;
 
 const MessageScreen = ({ route, navigation }: MessageProps) => {
+  const { user } = useAuth();
   const { matchDetails } = route.params;
+  console.log(matchDetails, user);
+  const [matchedUserInfo, setMatchedUserInfo] = useState<Profile>();
+
+  useEffect(() => {
+    (async function () {
+      const otherUserId = matchDetails.usersMatched.filter(
+        (id) => id === user?.uid
+      );
+      setMatchedUserInfo(await getUserById(otherUserId[0]));
+    })();
+  }, [matchDetails, user]);
 
   return (
-    <View>
-      <Header title="Message" callEnabled />
+    <SafeAreaView>
+      <Header title={matchedUserInfo?.displayName || "User"} callEnabled />
       <Text>MessageScreen</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
