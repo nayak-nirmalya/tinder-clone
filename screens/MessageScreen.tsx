bf0efef1a57977235600be538ba240c68edf3a82
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-29 07:34:23 +0530

    added textInput for sending msg

--- screens/MessageScreen.tsx
+++ screens/MessageScreen.tsx
@@ -1,7 +1,7 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { View, Text } from "react-native";
 import React, { useEffect, useState } from "react";
+import { View, Text, TextInput } from "react-native";
 
 import useAuth from "../hooks/useAuth";
 import Header from "../components/Header";
@@ -18,8 +18,11 @@ type MessageProps = NativeStackScreenProps<RootStackParamList, "Message">;
 const MessageScreen = ({ route, navigation }: MessageProps) => {
   const { user } = useAuth();
   const { matchDetails } = route.params;
-  console.log(matchDetails, user);
+
+  const [input, setInput] = useState("");
   const [matchedUserInfo, setMatchedUserInfo] = useState<Profile>();
+
+  const sendMessage = () => {};
 
   useEffect(() => {
     (async function () {
@@ -34,6 +37,16 @@ const MessageScreen = ({ route, navigation }: MessageProps) => {
     <SafeAreaView>
       <Header title={matchedUserInfo?.displayName || "User"} callEnabled />
       <Text>MessageScreen</Text>
+
+      <View>
+        <TextInput
+          className="h-10 text-lg"
+          placeholder="Send Message..."
+          onChangeText={setInput}
+          onSubmitEditing={sendMessage}
+          value={input}
+        />
+      </View>
     </SafeAreaView>
   );
 };
